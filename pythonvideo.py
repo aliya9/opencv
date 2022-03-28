a127287c41b24f7c554648f7778f362184a2e7dc
Aliya <farisaliya2004@gmail.com>
2022-03-28 23:20:45 +0530

    .

---
+++ pythonvideo.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+print(cap.isOpened())
+
+while(cap.isOpened()):
+    ret, frame= cap.read()
+
+    gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame',gray)
+
+    k = cv2.waitKey(1)
+
+    if k == ord('q'):
+        break
+
+
+
+cap.release()
+cv2.destroyAllWindows()
