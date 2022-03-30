80a52c0d14f3a9e6c4574a59c7d086427f75cb83
Aliya <farisaliya2004@gmail.com>
2022-03-30 23:02:36 +0530

    .

--- drawingObjects.py
+++ drawingObjects.py
@@ -2,9 +2,19 @@ import numpy as np
 
 import cv2
 
+img = np.zeros([512, 512, 3], np.uint8)
+
 img = cv2.imread('lena.jpg',1)
 
 img = cv2.line(img, (0,0), (255,255), (255,0,0), 10)
+img = cv2.arrowedLine(img, (0,255), (255,255), (255,0,0), 10)
+
+img = cv2.rectangle(img, (380,0), (510,120), (0,0,255), 10)
+
+img = cv2.circle(img, (480,480), 63, (0,255,0), -1)
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+
+img = cv2.putText(img, 'OpenCV', (10,500), font, 4, (255,255,255), 10, cv2.LINE_AA)
 
 cv2.imshow('image', img)
 
